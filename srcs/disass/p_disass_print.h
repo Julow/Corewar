11d2f8f54812677c5ba0f0ebebc1e0f61ffd7426
jaguillo <juloo.dsi@gmail.com>
2016-06-22 10:25:21 +0200

    Norme

--- srcs/disass/p_disass_print.h
+++ srcs/disass/p_disass_print.h
@@ -6,7 +6,7 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/21 21:42:34 by juloo             #+#    #+#             */
-/*   Updated: 2016/06/21 21:47:18 by juloo            ###   ########.fr       */
+/*   Updated: 2016/06/22 10:22:09 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,26 @@
 ** ========================================================================== **
 */
 
-#define PRINT_INVALID_OP	C_RED "# Invalid op: %.2hhx" C_RESET
-#define PRINTR_INVALID_OP	"# Invalid op: %.2hhx"
+# define PRINT_INVALID_OP	C_RED "# Invalid op: %.2hhx" C_RESET
+# define PRINTR_INVALID_OP	"# Invalid op: %.2hhx"
 
-#define PRINT_INSTR_NAME	C_LGREEN "%-5s" C_RESET
-#define PRINTR_INSTR_NAME	"%s "
+# define PRINT_INSTR_NAME	C_LGREEN "%-5s" C_RESET
+# define PRINTR_INSTR_NAME	"%s "
 
-#define PRINT_ARG_REG		"r%u"
-#define PRINT_ARG_DIR		"%%" C_YELLOW "%d" C_RESET
-#define PRINT_ARG_IND		C_YELLOW "%d" C_RESET
+# define PRINT_ARG_REG		"r%u"
+# define PRINT_ARG_DIR		"%%" C_YELLOW "%d" C_RESET
+# define PRINT_ARG_IND		C_YELLOW "%d" C_RESET
 
-#define PRINTR_ARG_REG		"r%u"
-#define PRINTR_ARG_DIR		"%%%d"
-#define PRINTR_ARG_IND		"%d"
+# define PRINTR_ARG_REG		"r%u"
+# define PRINTR_ARG_DIR		"%%%d"
+# define PRINTR_ARG_IND		"%d"
 
-#define PRINT_STRING		".%ts " C_LYELLOW "\"%ts\"" C_RESET "%n"
-#define PRINTR_STRING		".%ts \"%ts\"%n"
+# define PRINT_STRING		".%ts " C_LYELLOW "\"%ts\"" C_RESET "%n"
+# define PRINTR_STRING		".%ts \"%ts\"%n"
 
 /*
 ** -
 */
-#define PRINT(F,S)		(((F) & DISASS_F_RAW) ? PRINTR_##S : PRINT_##S)
+# define PRINT(F,S)		(((F) & DISASS_F_RAW) ? PRINTR_##S : PRINT_##S)
 
 #endif
